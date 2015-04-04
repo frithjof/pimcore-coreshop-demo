61a97db63b0030038c4141ca363f138ae83b5b00
Dominik Pfaffenbauer <dominik@lineofcode.at>
2015-04-04 09:42:31 +0200

    added profile page

---
+++ website/views/scripts/coreshop/user/settings.php
@@ -0,0 +1,15 @@
+<div id="main-container" class="container">
+    <!-- Breadcrumb Starts -->
+    <ol class="breadcrumb">
+        <li><a href="<?=$this->url(array("lang" => $this->language), "coreshop_index", true)?>"><?=$this->translate("Home")?></a></li>
+        <li><a href="<?=$this->url(array("lang" => $this->language, "action" => "profile"), "coreshop_user")?>"><?=$this->translate("My Profile")?></a></li>
+        <li class="active"><a href="<?=$this->url(array("lang" => $this->language, "action" => "settings"), "coreshop_user")?>"><?=$this->translate("Settings")?></a></li>
+    </ol>
+    <!-- Breadcrumb Ends -->
+    <!-- Main Heading Starts -->
+    <h2 class="main-heading text-center">
+        <?=$this->translate("Settings")?>
+    </h2>
+    <!-- Main Heading Ends -->
+
+</div>
